f37bdc73d3e11abba4dd7f32126658828f646381
wacwestley30 <westleycervantes@gmail.com>
2024-03-07 03:41:21 -0500

    index.js has a generateSVG function working and producing code in my examples folder. Need to fix the text that is being generated. Probably going to refactor the shapes.js to do a little more work.

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { Circle, Square, Triangle } = require('./lib/shapes');
+
+async function generateSVG() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters for the logo:',
+            validate: value => value.length <= 3 || 'Please enter up to three characters.'
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter text color or hexidecimal:'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape:',
+            choices: ['circle', 'square', 'triangle']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter shape color or hexidecimal:'
+        }
+    ]);
+
+    const { text, textColor, shape, shapeColor } = answers;
+
+    const shapeClasses = {
+        circle: Circle,
+        square: Square,
+        triangle: Triangle
+    };
+
+    const ShapeClass = shapeClasses[shape];
+
+    if(!ShapeClass) {
+        console.log('Invalid shape choice')
+        return;
+    }
+
+    const newShapeClass = new ShapeClass(shapeColor);
+
+    const svgMarkup = `
+        <svg width='300' height='200'>
+            ${newShapeClass.createShape()}
+            <text x='150' y='105' fill='${textColor}'>${text}</text>
+        </svg>
+    `;
+
+    fs.writeFileSync('./examples/logo.svg', svgMarkup);
+    console.log('Generated logo.svg');
+}
+
+generateSVG();
